69a1c6c0a3e1298202ed757af224042c028c69ce
kei10in <kei10in@gmail.com>
2018-07-13 00:45:00 +0900

    Implement class template for strongly typed guid

--- src/w32/guid.hpp
+++ src/w32/guid.hpp
@@ -6,4 +6,24 @@ namespace w32 {
 
 using guid = GUID;
 
+template <class T>
+class tagged_guid : public guid {
+ public:
+  constexpr tagged_guid() noexcept : guid{} {}
+
+  tagged_guid(tagged_guid const& pf) noexcept = default;
+  tagged_guid& operator=(tagged_guid const& pf) noexcept = default;
+
+  tagged_guid(tagged_guid&& pf) noexcept = default;
+  tagged_guid& operator=(tagged_guid&& pf) noexcept = default;
+
+  constexpr explicit tagged_guid(guid const& guid_value) noexcept
+      : guid{guid_value} {}
+
+  tagged_guid& operator=(guid const& guid_value) noexcept {
+    guid = guid_value;
+    return *this;
+  }
+};
+
 }  // namespace w32
